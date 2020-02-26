f39ca17386376d9b90c08d69b38c5f11bced51bd
schatzopoulos <s.chatzopoulos@gmail.com>
2020-02-26 13:32:35 +0200

    Read params from config file

--- src/main/java/athenarc/imsi/sdl/similarityjoin/Main.java
+++ src/main/java/athenarc/imsi/sdl/similarityjoin/Main.java
@@ -1,6 +1,10 @@
 package athenarc.imsi.sdl.similarityjoin;
 
+import java.io.FileReader;
 import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import athenarc.imsi.sdl.similarityjoin.algorithms.SimilarityJoinAlgorithm;
 import athenarc.imsi.sdl.similarityjoin.algorithms.SimilarityMeasure;
@@ -9,16 +13,33 @@ import athenarc.imsi.sdl.similarityjoin.algorithms.topk.TopKQueue;
 
 public class Main {
     public static void main(String [] args) {
-        
-        String inputfile = "/opt/workflows/data/out/HIN.csv";
-        String outfile = "/opt/workflows/data/out/TMP.csv";
-        int t = 1;
-        int w = 0;
-        int minValues = 5;
+
+        // check params
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar SimilarityJoin.jar -c <json config file>");
+            System.exit(1);
+        }
+
+        // read config json
+        JSONObject config = null;
+        try {
+            JSONParser parser = new JSONParser();
+            
+             config = (JSONObject) parser.parse(new FileReader(args[1]));
+
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        String inputfile = (String) config.get("hin_out");
+        String outfile = (String) config.get("analysis_out");
+        int k = ((Long)config.get("k")).intValue();
+        int t = ((Long)config.get("t")).intValue();
+        int w = ((Long)config.get("w")).intValue();
+        int minValues = ((Long)config.get("min_values")).intValue();
 
         SimilarityJoinAlgorithm algorithm = new PSJoin(t, w, minValues);
-
-        int k = 100;
 
         algorithm.setK(k);
         algorithm.setSimilarityMeasure(SimilarityMeasure.TYPE.JOIN_SIM);
@@ -30,6 +51,7 @@ public class Main {
        TopKQueue topK = algorithm.execute();
 
        Utils.writeProgress(4, "Writing Results");
+
        try {
             topK.write(outfile);
        } catch (IOException e) {
