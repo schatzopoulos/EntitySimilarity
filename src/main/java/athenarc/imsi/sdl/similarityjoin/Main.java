eabf6053215da33a0be036957eec4baefb4ae9c5
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2020-10-30 11:51:57 +0200

    Log all similarities without LSH

--- src/main/java/athenarc/imsi/sdl/similarityjoin/Main.java
+++ src/main/java/athenarc/imsi/sdl/similarityjoin/Main.java
@@ -1,8 +1,10 @@
 package athenarc.imsi.sdl.similarityjoin;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import athenarc.imsi.sdl.similarityjoin.algorithms.baseline.Baseline;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -16,69 +18,36 @@ public class Main {
 
         // check params
         if (args.length != 4) {
-            System.out.println("Usage: java -jar EntitySimilarity.jar -c <json config file> <analysis> <hin_in>");
+            System.out.println("Usage: java -jar EntitySimilarity.jar <hin_in> <author_ids> <min_values> <similarityThreshold>");
             System.exit(1);
         }
 
-        // read config json
-        JSONObject config = null;
-        try {
-            JSONParser parser = new JSONParser();
-            
-             config = (JSONObject) parser.parse(new FileReader(args[1]));
 
-        } catch (Exception e) {
-            System.out.println(e);
-            System.exit(1);
-        }
+        String hinInput = (String) args[0];
+        String authorIdsFile = (String) args[1];
+        int minValues = Integer.parseInt(args[2]);
+        double simThreshold = Double.parseDouble(args[3]);
 
-        String analysis = (String) args[2];
-        String inputfile = (String) args[3];
-
-        if (!analysis.equals("Similarity Join") && !analysis.equals("Similarity Search")) {
-            System.exit(-1);
-        }
-
-        int t = ((Long)config.get("t")).intValue();
-        int w = -1;
-        int k = -1;
-        int minValues = -1;
-        
-        if (analysis.equals("Similarity Search")) {
-            k = ((Long)config.get("searchK")).intValue();
-            w = ((Long)config.get("searchW")).intValue();
-            minValues = ((Long)config.get("searchMinValues")).intValue();
-        } else {
-            k = ((Long)config.get("joinK")).intValue();
-            w = ((Long)config.get("joinW")).intValue();
-            minValues = ((Long)config.get("joinMinValues")).intValue();        
-        }
-
-        SimilarityJoinAlgorithm algorithm = new PSJoin(analysis, t, w, minValues);
-        algorithm.setK(k);
+        SimilarityJoinAlgorithm algorithm = new Baseline(simThreshold, minValues);
         algorithm.setSimilarityMeasure(SimilarityMeasure.TYPE.JOIN_SIM);
+        algorithm.readRelationMatrix(hinInput);
 
-        algorithm.readRelationMatrix(inputfile);
 
-        // algorithm.printRelationMatrix();
-        TopKQueue topK = null;
-        String outfile = null;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(authorIdsFile));
+            String line = reader.readLine();
+            while (line != null) {
+//                System.out.println(line);
 
-        if (analysis.equals("Similarity Join")) {
-            topK = algorithm.execute();
-            outfile = (String) config.get("sim_join_out");
-        } else if (analysis.equals("Similarity Search")) {
-            int targetId = ((Long)config.get("target_id")).intValue();
-            topK = algorithm.executeSearch(targetId);
-            outfile = (String) config.get("sim_search_out");
+                algorithm.executeSearch(Integer.parseInt(line));
+
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        Utils.writeProgress(analysis, 3, "Writing Results");
-        
-        try {
-            topK.write(outfile);
-        } catch (IOException e) {
-            System.out.println(e);
-        }  
     }
 }
--- src/main/java/athenarc/imsi/sdl/similarityjoin/algorithms/baseline/Baseline.java
+++ src/main/java/athenarc/imsi/sdl/similarityjoin/algorithms/baseline/Baseline.java
@@ -6,14 +6,16 @@ import athenarc.imsi.sdl.similarityjoin.algorithms.SimilarityMeasure;
 import athenarc.imsi.sdl.similarityjoin.algorithms.relationmatrix.SparseVector;
 import athenarc.imsi.sdl.similarityjoin.algorithms.topk.TopKQueue;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Baseline extends SimilarityJoinAlgorithm {
 
     private long topKTime = 0;
+    private int minValues = -1;
+    private double simThreshold = -1;
+
+    public Baseline(double simThreshold, int minValues) {
+        this.simThreshold = simThreshold;
+        this.minValues = minValues;
+    }
 
     @Override
     public TopKQueue execute() {
@@ -26,6 +28,31 @@ public class Baseline extends SimilarityJoinAlgorithm {
 
     @Override
     public TopKQueue executeSearch(int authorId) {
+
+        RelationMatrix matrix = super.getRelationMatrix();
+        SparseVector authorRow = matrix.getRow(authorId);
+        if (authorRow == null)
+            return null;
+
+        double authorNorm = authorRow.norm2();
+
+        for (int i=0; i<matrix.getRowsLength(); i++) {
+
+            SparseVector currentRow = matrix.getRow(i);
+
+            // bypass rows with less than minValues
+            if (currentRow == null
+                    || authorId == i
+                    || currentRow.getUsed() < this.minValues)
+                continue;
+
+            double similarity = SimilarityMeasure.calculate(authorRow, currentRow, authorNorm, super.getSimilarityMeasure());
+
+            if (!Double.isNaN(similarity) && similarity >= this.simThreshold) {
+                System.out.println(authorId + "\t" + i + "\t" + similarity);
+            }
+        }
+
         return null;
     }
 
